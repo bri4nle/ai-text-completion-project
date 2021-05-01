3e8e20f4a03f9bb8ba380ff3932efc9e1e3c4c57
Brian Le <lblt1995@gmail.com>
2021-05-01 16:21:33 -0500

    complete

--- file_io.py
+++ file_io.py
@@ -1,9 +1,16 @@
 class FileIO:
+    """
+    This class provides file IO
+    """
     def __init__(self, file_name):
         self.content = ''
         self.__file_name = file_name
 
     def read(self):
+        """
+        This function read the file input file and return its content
+        :return:
+        """
         with open(self.__file_name, encoding='utf8', mode="r") as f:
             self.content += f.read()
         return self.content
--- user_interface.py
+++ user_interface.py
@@ -9,6 +9,10 @@ class UserInterface:
         self.text_gen = text_gen
 
     def runProgram(self):
+        """
+        This function provides an command line interface for users.
+        :return:
+        """
         self.instructions  # print instructions
 
         while True:
@@ -18,11 +22,14 @@ class UserInterface:
             while True:
                 word_seq = self.get_last_2_words(self.user_input)
                 predicted_words = self.text_gen.generate_next_words(word_seq)
-                if not predicted_words:  # AI couldn't come up with predicted words
+                if not predicted_words:  # AI couldn't comes up with predicted words
                     # print('*** AI looses points ***')
                     new_user_input = input(self.user_input)
-                    if new_user_input[-1] == ".":
+                    if new_user_input == "":
+                        pass
+                    elif new_user_input[-1] == ".":
                         self.user_input += new_user_input
+                        # Take the user's new input and update the model
                         self.text_gen.update(self.user_input.split(' '))
                         break
                     self.user_input += new_user_input
@@ -33,6 +40,8 @@ class UserInterface:
                     index = 0
                     user_entered_new_words = False
                     done = False
+                    do_nothing = False
+                    # Displays the predicted words
                     for i, word in enumerate(predicted_words):
                         print('%d: %s\t' % (i + 1, word)),
                         if i == 2:
@@ -40,8 +49,12 @@ class UserInterface:
                     while True:  # Input validation
                         try:
                             user_choice = input(self.user_input)
+                            if user_choice == "":
+                                do_nothing = True
+                                break
                             if user_choice[-1] == ".":
                                 self.user_input += user_choice
+                                # Take the user's new input and update the model
                                 self.text_gen.update(self.user_input.split(' '))
                                 done = True
                                 break
@@ -59,18 +72,34 @@ class UserInterface:
                         # AI looses point
                         self.user_input += user_choice
                         self.total_num_picked += 1
+                    elif do_nothing:
+                        pass
                     else:  # The predicted list has the desired word, appends that word to the sentence.
                         self.user_input += ' ' + predicted_words[index - 1]
                         self.total_correct_pick += 1
                         self.total_num_picked += 1
+                        if self.user_input[:-1] == ".":
+                            break
                 print('-----------------------------------------------------')
-                print('AI performance rate: {:.2}'.format(self.total_correct_pick / self.total_num_picked))
+                try:
+                    print('AI performance rate: {:.2}'.format(self.total_correct_pick / self.total_num_picked))
+                except ZeroDivisionError as e:
+                    pass
 
     def get_last_2_words(self, user_input):
+        """
+        This function extract the last 2 word of the input and returns them.
+        :param user_input:
+        :return:
+        """
         string_vector = user_input.split()[-2:]
         return ' '.join(string_vector)
 
     def instructions(self):
+        """
+        Instruction message.
+        :return:
+        """
         print("""
                     *** This is an AI Text Completion Program ***
 
